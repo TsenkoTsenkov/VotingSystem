3a972269dbe4b246e9fe24a24aed533da4f683e0
TsenkoTsenkov <tseni.tsenkov@gmail.com>
2020-03-23 22:52:37 +0200

    Removed old files

--- VotingOrganiser/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Organise = artifacts.require("Organise");
-const YesNoVoting = artifacts.require("YesNoVoting");
-
-module.exports = function(deployer) {
-  deployer.deploy(Organise);
-  deployer.deploy(YesNoVoting);
-};
